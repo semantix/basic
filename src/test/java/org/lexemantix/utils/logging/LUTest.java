2aa504ab0e3e8c398129e6633a1cb6fe52747706
Deepak Sharma <sharma.deepak2@mayo.edu>
2015-06-23 21:52:54 -0500

    Added logging using logback/slf4j

--- src/test/java/org/lexemantix/utils/logging/LUTest.java
+++ src/test/java/org/lexemantix/utils/logging/LUTest.java
@@ -2,6 +2,8 @@ package org.lexemantix.utils.logging;
 
 import junit.framework.TestCase;
 
+import java.util.Date;
+
 /**
  * Created by dks02 on 6/23/15.
  */
@@ -14,39 +16,24 @@ public class LUTest extends TestCase
         logger.trace("This is a test Trace 1");
     }
 
-    public void testTrace1() throws Exception {
-
-    }
-
-    public void testTrace2() throws Exception {
-
-    }
-
-    public void testTrace3() throws Exception {
-
-    }
-
-    public void testTrace4() throws Exception {
-
-    }
-
     public void testDebug() throws Exception {
-
+        logger.debug("This is {} debug", "FIRST");
     }
 
-    public void testDebug1() throws Exception {
-
+    public void testWarn() throws Exception {
+        logger.warn("This one has an exception", new Exception("Mock Exception"));
     }
 
-    public void testDebug2() throws Exception {
-
+    public void testInfo() throws Exception {
+        logger.info("Testing info with integer value={}", 1000);
     }
 
-    public void testDebug3() throws Exception {
-
+    public void testError() throws Exception {
+        logger.error("Testing error with 2 parameters int={} and date={}", 556, new Date());
     }
 
-    public void testDebug4() throws Exception {
-
+    public void testOff() throws Exception {
+        //logger.setLevel(Level.DEBUG.OFF);
+        logger.error("This should not get printed as level was set to OFF, check your code...");
     }
 }
